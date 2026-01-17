433fb190903f8a4cc6f5b4a0b1854ec3c346f7ba
Trần Doãn Nhất <trandoannhat@yahoo.com>
2026-01-17 15:12:33 +0700

    edit yml

--- AdminApi/Program.cs
+++ AdminApi/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<JwtService>();
+////
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 .AddJwtBearer(options =>
